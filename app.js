da34dcc66a843fc18b2bb366b666c55e2b423dae
Stephanie McCandless <mccandlessstephi@gmail.com>
2020-11-02 20:09:49 -0800

    Added apiKey and funtion

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+const apiKey = '9795c05e3861e29aa3fa17430c07e4d2';
+const appId = "&appid=";
+
+
+$("#btnSrc").on("click", function() {
+    let searchCity = $(".form-control").val();
+    let weatherSrc = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + appId + apiKey;
+
+    $.ajax({
+        url: weatherSrc,
+        method: "GET"
+    }).then(function (res) {
+        console.log(res);
+        let cityName = res.name;
+        console.log(cityName);
+        $("#cityName").text(cityName);
+    })
+
+
+})
