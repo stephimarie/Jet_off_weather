654c2f14a8624494505816b32b8c3854a3ad1fd7
Stephanie McCandless <mccandlessstephi@gmail.com>
2020-11-07 18:23:13 -0800

    Add condition

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const appId = "&appid=";
 
 $("#btnSrc").on("click", function() {
     let searchCity = $(".input").val();
-    let weatherSrc = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + appId + apiKey;
+    let weatherSrc = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity+ "&units=imperial" + appId + apiKey;
     console.log(searchCity)
 
     $.ajax({
@@ -15,8 +15,11 @@ $("#btnSrc").on("click", function() {
     }).then(function (res) {
         console.log(res);
         let cityName = res.name;
-        console.log(cityName);
+    
+        let temp = res.main.feels_like
+         console.log(temp);
         $("#cityName").text(cityName);
+        $("#temp").text(temp)
     })
 
 
